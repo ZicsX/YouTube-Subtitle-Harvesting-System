b43ce940b5a6408e64655410f5025c48fa96711c
Satish Mishra <zicsxone@gmail.com>
2023-06-27 20:05:48 +0530

    updated tasks as per YouTubeAPI

--- harvester/tasks.py
+++ harvester/tasks.py
@@ -1,27 +1,57 @@
-from __future__ import absolute_import, unicode_literals
+# harvester/tasks.py
 from celery import shared_task
-from youtube.connector import search_youtube_videos, get_random_sentence_from_subtitle
-from youtube.downloader import download_subtitles
+from youtubeapi.youtube import YouTubeAPI
+from youtubeapi.downloader import YouTubeSubtitleDownloader
 from .models import Video, Query
+from django.db import IntegrityError
+import random
 
-@shared_task(bind=True, max_retries=3)
-def search_youtube(self, query):
-    try:
-        video_ids = search_youtube_videos(query)
-        for video_id in video_ids:
-            download_and_save_subtitle.delay(video_id)  # Queue a task for each video
-    except Exception as exc:
-        raise self.retry(exc=exc)
+youtube_api = YouTubeAPI()
+subtitle_downloader = YouTubeSubtitleDownloader()
 
-@shared_task(bind=True, max_retries=3)
-def download_and_save_subtitle(self, video_id):
+
+@shared_task()
+def search_and_download():
+    # Fetching a random query
+    query = Query.objects.order_by('?').first()
+    if query is None:
+        print("No query found in the database. Please add queries to start harvesting subtitles.")
+        return
+
     try:
-        subtitle = download_subtitles(video_id)
-        if subtitle is not None:  # Make sure we have a subtitle
-            video = Video(video_id=video_id, subtitle=subtitle)
-            video.save()  # Saving the video and subtitle to the database
-            sentence = get_random_sentence_from_subtitle(subtitle)
-            new_query = Query(query=sentence)
-            new_query.save()  # Saving the new query to the database
-    except Exception as exc:
-        raise self.retry(exc=exc)
+        # Searching for videos using the query
+        video_ids = youtube_api.search_videos(query.query)
+    except Exception as e:
+        print(f"Error occurred while searching videos: {e}")
+        return
+
+    for video_id in video_ids:
+        # Checking if the video already exists in the database
+        video_exists = Video.objects.filter(video_id=video_id).exists()
+        if video_exists:
+            print(f"Video ID {video_id} already exists in the database.")
+            continue
+
+        try:
+            # Checking if the video has Hindi captions
+            if youtube_api.check_hindi_captions(video_id):
+                # Downloading the subtitles
+                subtitles = subtitle_downloader.download_subtitle(video_id)
+                # Storing the video and its subtitles
+                Video.objects.create(video_id=video_id, subtitle=subtitles)
+
+                # Generating a new query using a random sentence from the subtitles
+                sentences = subtitles.split(" ... ")
+                new_query = random.choice(sentences)
+                Query.objects.create(query=new_query)
+
+        except Exception as e:
+            print(f"Error occurred while processing video ID {video_id}: {e}")
+            continue
+
+    try:
+        # Deleting the used query
+        query.delete()
+    except Exception as e:
+        print(f"Error occurred while deleting query: {e}")
+
