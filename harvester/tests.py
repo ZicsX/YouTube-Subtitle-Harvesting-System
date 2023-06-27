0dd65225e06bd3d64467d98bf0bc2434601c6b58
Satish Mishra <zicsxone@gmail.com>
2023-06-27 14:37:39 +0530

    added tests

--- harvester/tests.py
+++ harvester/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from harvester.models import Query, Video
+from harvester.tasks import search_youtube, download_and_save_subtitle
+import time
 
-# Create your tests here.
+class CeleryTasksTestCase(TestCase):
+
+    def setUp(self):
+        # Create a test Query and Video object for testing
+        Query.objects.create(query='हिंदी समाचार')
+        Video.objects.create(video_id='DDfE7bGxIug', subtitle='हिंदी समाचार!!!')
+
+    def test_search_youtube(self):
+        query = Query.objects.first().query
+        task = search_youtube.apply_async(args=(query,))  # apply_async() dispatches the task to the Celery worker
+        while not task.ready():  # Wait until the task has been processed
+            time.sleep(1)
+        self.assertIsNotNone(task.result)  # The result attribute contains the task's result
+
+    def test_download_and_save_subtitle(self):
+        video_id = Video.objects.first().video_id
+        task = download_and_save_subtitle.apply_async(args=(video_id,))
+        while not task.ready():
+            time.sleep(1)
+        self.assertIsNotNone(task.result)
