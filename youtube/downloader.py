decbad34450152bce3d73a5217260c0ad33501cf
Satish Mishra <zicsxone@gmail.com>
2023-06-27 20:02:44 +0530

    remove test scripts

--- youtube/downloader.py
+++
@@ -1,9 +0,0 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-
-def download_subtitles(video_id):
-    try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['hi'])
-        full_transcript = ' ... '.join(entry['text'] for entry in transcript)
-        return full_transcript
-    except:
-        return None
