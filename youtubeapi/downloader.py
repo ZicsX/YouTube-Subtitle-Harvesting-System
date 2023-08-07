455254616a6d46f833a5861c858ff3f5c675c25a
Satish Mishra <zicsxone@gmail.com>
2023-08-08 00:22:16 +0530

    updated downloader

--- youtubeapi/downloader.py
+++ youtubeapi/downloader.py
@@ -20,22 +20,16 @@ class YouTubeSubtitleDownloader:
                 transcript = transcripts.find_manually_created_transcript(['hi']).fetch()
                 full_transcript = " ... ".join(entry["text"] for entry in transcript)
                 return full_transcript
-            except NoTranscriptFound as e:
-                tr = 0
+            except NoTranscriptFound:
                 for transcript in transcripts:
                     data_dict = {data["language_code"]: data for data in transcript.translation_languages}
                     is_present = "hi" in data_dict
                     if is_present:
-                        tr  = transcript
-                        break
-                if tr:
-                    try:
-                        transcript =  tr.translate('hi').fetch()
-                        full_transcript = " ".join(entry["text"] for entry in transcript)
-                        return full_transcript
-                    except TranslationLanguageNotAvailable:
-                        return False
-                else:
-                    return False
+                        try:
+                            full_transcript = " ".join(entry["text"] for entry in transcript.translate('hi').fetch())
+                            return full_transcript
+                        except TranslationLanguageNotAvailable:
+                            return False
+                return False
         except TranscriptsDisabled:
             return False
