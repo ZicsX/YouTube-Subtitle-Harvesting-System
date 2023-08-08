09c29e18426c3c0da98d84192fce205e55b719fa
Satish Mishra <zicsxone@gmail.com>
2023-08-08 09:46:03 +0530

    optimized DB calls

--- harvester/tasks.py
+++ harvester/tasks.py
@@ -20,33 +20,35 @@ def search_videos(query):
         return []
 
 
+from django.db.utils import IntegrityError
+
 @shared_task
 def download_subtitle_for_video(video_id, text):
-    # Checking if the video already exists in the database
-    video_exists = Video.objects.filter(video_id=video_id).exists()
-    if video_exists:
-        logger.info(f"Video ID {video_id} already exists in the database.")
-        return
-
     try:
         subtitles = subtitle_downloader.download_subtitle(video_id)
         if subtitles:
             categories = tagger.tag_string(text)
-            Video.objects.create(
-                video_id=video_id, subtitle=subtitles, categories=categories
-            )
             new_query = tagger.random_sentence(subtitles)
-            Query.objects.create(query=new_query)
+
+            try:
+                Video.objects.create(video_id=video_id, subtitle=subtitles, categories=categories)
+                Query.objects.create(query=new_query)
+            except IntegrityError:
+                logger.info(f"Video ID {video_id} already exists in the database.")
         else:
-            NoSubtitle.objects.create(video_id=video_id)
+            try:
+                NoSubtitle.objects.create(video_id=video_id)
+            except IntegrityError:
+                logger.info(f"Video ID {video_id} is already marked as NoSubtitle in the database.")
     except Exception as e:
         logger.error(f"Error occurred while downloading CC of video ID {video_id}: {e}")
 
 
 @shared_task()
 def search_and_download():
-    # Check the system state
+    # Fetch system state
     state = SystemState.objects.first()
+
     if state is None or not state.is_running:
         return
 
