4e077deb42c1eb55372c51bb155ed51a65946162
Satish Mishra <zicsxone@gmail.com>
2023-08-08 10:51:34 +0530

    Setting up caching

--- web_interface/settings.py
+++ web_interface/settings.py
@@ -197,3 +197,11 @@ LOGGING = {
         },
     },
 }
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
