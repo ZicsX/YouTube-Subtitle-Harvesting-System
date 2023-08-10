b41bb5ba786ba922c9f533d49fdd7750012b4e8d
Satish Mishra <zicsxone@gmail.com>
2023-08-10 16:58:05 +0530

    nu

--- manage.py
+++ manage.py
@@ -2,53 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import atexit
-from subprocess import Popen, DEVNULL, STDOUT
-from web_interface.settings import WINDOWS, CELERY_CONCURRENCY
-
-
-def start_service(args):
-    return Popen(args, stdout=DEVNULL, stderr=STDOUT)
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_interface.settings")
-
-    celery_args = [
-        "celery",
-        "-A",
-        "web_interface",
-        "worker",
-        "--logfile=logs/celery.log",
-    ]
-
-    if WINDOWS:
-        celery_args.extend(["--pool=solo"])
-
-    else:
-        if CELERY_CONCURRENCY:
-            celery_args.extend(["--concurrency", str(CELERY_CONCURRENCY)])
-
-    #  Start celery worker
-    celery_process = start_service(celery_args)
-
-    def exit_handler():
-        """Stop the Celery worker and set is_running to False."""
-
-        if WINDOWS:
-            if celery_process.poll() is None:
-                celery_process.terminate()
-                celery_process.wait()
-        else:
-            os.system("pkill -f 'celery'")
-
-        from harvester.utils.cache_utils import set_system_state
-
-        set_system_state(False)
-
-    atexit.register(exit_handler)
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
